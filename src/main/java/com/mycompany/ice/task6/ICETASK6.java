bcee8bf902265fe2b2aeb962b7f18e94b9add518
RC_Student_lab <RC_Student_lab@10.0.0.18>
2024-09-19 06:45:57 +0000

    Added a scanner object, declared a string and prompted user to write a sentence

---
+++ src/main/java/com/mycompany/ice/task6/ICETASK6.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ice.task6;
+import java.util.Scanner;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class ICETASK6 {
+
+    
+   
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        String sentence;
+        
+        //Prompt user to input 
+        System.out.println("Write a sentence");
+        sentence = input.next();
+    }
+}
